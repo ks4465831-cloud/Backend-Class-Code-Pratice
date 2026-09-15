6a0c7cf078d46890d51a5a3085fbda1b8950c79d
ks4465831-cloud <ks4465831@gmail.com>
2026-09-15 21:51:33 +0530

    feat :added http code

--- lecture 6/script.js
+++ lecture 6/script.js
@@ -1,27 +1,10 @@
-console.log("Hello, World!");
+const process=require('process');
+// console.log("Hello ")
 
+const data=process.argv
+// console.log(Number(data[2])+Number(data[3]));
 
-const process = require('process');
-const data = process.argv
-
-
-console.log(data);
-
-console.log(data[2]); 
-
-
-console.log(data[2]+" "+data[3]);
-
-console.log(Number(data[2])+Number(data[3]));
-
-console.log(process.version);
-
-console.log(process.platform);
-
-console.log(process.arch);
-
-console.log(process.pid);
-
-console.log(process.cwd());
-
-
+console.log(process.version)
+console.log(process.platform)
+console.log(process.pid)
+console.log(process.cwd())
