ae360a8373a23e36d86c6d98fbb8ecd4743ca502
ks4465831-cloud <ks4465831@gmail.com>
2026-09-17 19:41:03 +0530

    feat : update server

--- lecture 12/server.js
+++ lecture 12/server.js
@@ -1,64 +1,34 @@
 const express = require("express");
 
+const students = require("./data/studentData");
+
+const studentController = require("./controller/studentController");
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-const students = [
-    { rollNo: 1, name: "Abhijeet", section: "BCA-D" },
-    { rollNo: 2, name: "Upen", section: "BCA-D" },
-    { rollNo: 3, name: "Rahul", section: "BCA-D" },
-    { rollNo: 4, name: "Gurpreet", section: "BCA-D" },
-    { rollNo: 5, name: "Daulat", section: "BCA-D" }
-];
+app.use("/students",studentRoutes);
+
 
 // Read Operation
-app.get("/students", (req, res) => {
-    res.json(students);
-});
+// app.get("/students", studentController.getStudents);
 
 // Read operation with id
-app.get("/students/:rollNo", (req, res) => {
-    const id = req.params.rollNo;
-    const student = students.find(s => s.rollNo === parseInt(id));
-    if (!student) {
-        return res.status(404).json({ error: "Student not found" });
-    }
-    res.json(student);
-});
+// app.get("/students/:rollNo", studentController.getStudentById);
 
 //create operation
 
-app.post("/students", (req, res) => {
-   const data = req.body;
-   students.push(data);
-   res.json({ student: true, data });
-});
+// app.post("/students", studentController.createStudent);
 
 
 //update operation
-app.put("/students/:rollNo", (req, res) => {
-    const id = req.params.rollNo;
-    const data = req.body;
-    const student=students.find((student) => student.rollNo === Number(id));
-    if (!student) {
-      res.status(404).json({ success: false, message: "Student not found" });
-    }
-    student.name = data.name;
-    student.section = data.section; 
-    res.json({ success:true, student });
-});
+// app.put("/students/:rollNo", studentController.updateStudent);
 
-    // detele operation
-    app.delete("/students/:rollNo", (req, res) => {
-        const id = req.params.rollNo;
-        const studentIndex = students.findIndex((student) => student.rollNo === Number(id));
-        if (studentIndex === -1) {
-            res.status(404).json({ success: false, message: "Student not found" });
-        } else {
-        students.splice(studentIndex, 1);
-        res.json({ success: true, message: "Student deleted successfully" });
+//delete operation
+// app.delete("/students/:rollNo", studentController.deleteStudent);
+   
 
-}});
+  
 app.listen(PORT, () => console.log("server is running"));
