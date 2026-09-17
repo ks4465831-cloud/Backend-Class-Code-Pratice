d2fa6cd04e194005bb7bad28da79f4bb3b64bc7f
ks4465831-cloud <ks4465831@gmail.com>
2026-09-17 19:33:48 +0530

    feat : added data

---
+++ lecture 12/data/studentData.js
@@ -0,0 +1,9 @@
+const students = [
+    {rollNo: 1, name: "John Doe", section: "D"},
+    {rollNo: 2, name: "Jane Smith", section: "D"},
+    {rollNo: 3, name: "Alice Johnson", section: "D"},
+    {rollNo: 4, name: "Bob Brown", section: "D"},
+    {rollNo: 5, name: "Charlie Davis", section: "D"}
+];
+
+module.exports = students;
