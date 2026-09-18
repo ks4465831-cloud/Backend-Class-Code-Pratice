edd4fd5e3910bf9985fc2ec9f3e8cdb28a0b113b
ks4465831-cloud <ks4465831@gmail.com>
2026-09-18 21:20:38 +0530

    feat : update server

--- lecture 7/server.js
+++ lecture 7/server.js
@@ -1,10 +1,10 @@
 const http=require("http");
 
 const users=[
-    {id:1, name:"Devendra", email:"GZDlC@example.com"},
-    {id:2, name:"Ravi", email:"ravi@com"},
-    {id:3, name:"alex", email:"alex@com"},
-    {id:4, name:"vasu", email:"vasu@com"},
+    {id:1, name:"Khushi", email:"GZDlC@example.com"},
+    {id:2, name:"Shikha", email:"ravi@com"},
+    {id:3, name:"John", email:"alex@com"},
+    {id:4, name:"Vaibhav", email:"vasu@com"},
 ]
 
 const server=http.createServer((req, res) => {
