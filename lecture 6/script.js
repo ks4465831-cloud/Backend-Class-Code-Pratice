84f02c09e57945fe5c1d25f5b51a1bae79ad194a
ks4465831-cloud <ks4465831@gmail.com>
2026-09-20 22:09:55 +0530

    feat : add http code

--- lecture 6/script.js
+++ lecture 6/script.js
@@ -2,7 +2,7 @@ const process=require('process');
 // console.log("Hello ")
 
 const data=process.argv
-// console.log(Number(data[2])+Number(data[3]));
+console.log(Number(data[2])+Number(data[3]));
 
 console.log(process.version)
 console.log(process.platform)
