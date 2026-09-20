b81d57051ad0340061cb8d35577ae2887c139917
ks4465831-cloud <ks4465831@gmail.com>
2026-09-20 22:03:26 +0530

    feat: added server

--- lecture 9/server.js
+++ lecture 9/server.js
@@ -4,10 +4,10 @@ const app=express()
 
 const PORT=3000
 const users=[
-    {id:1, name:"Devendra", email:"GZDlC@example.com"},
-    {id:2, name:"Ravi", email:"ravi@com"},
-    {id:3, name:"alex", email:"alex@com"},
-    {id:4, name:"vasu", email:"vasu@com"},
+    {id:1, name:"Devendra", email:"devendra@example.com"},
+    {id:2, name:"Ravi", email:"ravi@example.com"},
+    {id:3, name:"alex", email:"alex@example.com"},
+    {id:4, name:"vasu", email:"vasu@example.com"},
 ]
 
 app.get("/",(req,res)=>{
