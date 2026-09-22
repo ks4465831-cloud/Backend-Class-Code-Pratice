30bf21991b0c47ec77dd49cf9105921461f239fb
ks4465831-cloud <ks4465831@gmail.com>
2026-09-22 22:18:40 +0530

    feat : update server

--- lecture 18/server.js
+++ lecture 18/server.js
@@ -6,14 +6,21 @@ app.get('/', (req, res) => {
     try {
         throw new Error("Something went wrong!");
     } catch (error) {
-        res.status(500).json({success:false, message: "Something went wrong!"});
-       
+        // res.status(500).json({success:false, message: "Something went wrong!"});
+        next(error);   
+        // pass the error to thr error handling middleware
     }
 });
 
+// app.get('/', (req, res) => {
+//     try {
+//         throw new Error("Something went wrong!");
+//     } catch (error) {
+//         res.status(500).json({success:false, message: error.message});
+//     }
+// });
 
-
-app.get("/age-check/:age", (req, res) => {
+app.get("/age-check/:age", (req, res,next) => {
     // let age = 17;
     let age = req.params.age;
     try{
@@ -23,18 +30,36 @@ app.get("/age-check/:age", (req, res) => {
             res.send("You are eligible to vote");
         }
     }catch(error) {
-        res.status(500).json({success:false, message: "Age is less than 18"});
-        
+        // res.status(500).json({success:false, message: "Age is less than 18"});
+        next(error);
     }
 });
 
+// app.get("/age-check/:age", (req, res) => {
+//     // let age = 17;
+//     let age = req.params.age;
+//     try{
+//         if(age<18){
+//             throw new Error("You are not eligible to vote");
+//         }else{
+//             res.send("You are eligible to vote");
+//         }
+//     }catch(error) {
+//         res.status(500).json({success:false, message: error.message});
+        // next(error); 
 
+//     }
+// });
 
-app.use((req, res) => {
+app.use((err, req, res, next) => {
     res.status(500).json({success:false, message: err.message});
 });
 
 
-
+// invalid route middleware - ye humesha code ke last ma likhenge kyuki phale likhenge to ye sabse phale execute hoga aur ye 404 error dega
+// iske baad kuch bhi nhi chalta
+app.use((req, res) => {
+    res.status(404).json({success:false, message: "Route not found"});
+});
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
