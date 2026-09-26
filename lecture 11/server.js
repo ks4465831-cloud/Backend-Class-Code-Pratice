f409b25617a5e3469468347a286b37d923f5a4ec
ks4465831-cloud <ks4465831@gmail.com>
2026-09-26 22:49:55 +0530

    feat : added req,res code

--- lecture 11/server.js
+++ lecture 11/server.js
@@ -1,45 +1,33 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+const express=require("express")
+const app=express();
+const PORT=3000
 
-app.use(express.json());
-// ye ek middleware hai
+app.use(express.json());  //json data parse
 
-// app.get('/', (req, res) => {
-//     console.log("Request URL: ", req.url);
-//     console.log("Request Method: ", req.method);
-//     console.log("Request Headers: ", req.headers);
-    
-//     res.send('Hello Welcome to the get request!');
-// });
+app.get("/about",(req,res)=>{
+    // console.log("Request URL" ,req.url);
+    // console.log("Request Method" ,req.method);
+    // console.log("Request Headers" ,req.headers);
+    console.log("Request Query",req.query);
+    res.send("Welcome to the about page");
+})
 
-
-// app.get('/:id', (req, res) => {
-//     console.log("Request URL: ", req.url);
-//     console.log("Request Method: ", req.method);
-//     console.log("Request Headers: ", req.headers);
-//     console.log("Request Params: ", req.params.id);
-//     res.send('Hello Welcome to the get request!');
-// });
+app.get("/:id",(req,res)=>{    /// / /:id  bith are different
+    // console.log("Request URL" ,req.url);
+    // console.log("Request Method" ,req.method);
+    // console.log("Request Headers" ,req.headers);
+    console.log("Request Params:" ,req.params.id);
+    res.send("Welcome to the home page");
+})
 
 
-app.get('/about', (req, res) => {
-    // console.log("Request URL: ", req.url);
-    // console.log("Request Method: ", req.method);
-    // console.log("Request Headers: ", req.headers);
-    console.log("Request Query: ", req.query);
-    res.send('welcome to the about page!');
-});
+app.post("/",(req,res)=>{
+    // console.log("Request URL" ,req.url);
+    // console.log("Request Method" ,req.method);
+    console.log("Request Body:" ,req.body);
+    res.send("Welcome to the post route");
+})
 
-
-
-    // console.log("Request URL: ", req.url);
-    // console.log("Request Method: ", req.method);
-    // console.log("Request Headers: ", req.headers);
-//     console.log("Request Body: ", req.body);
-//     res.send('Hello Welcome to the post request!');
-// });
-
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
-});
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
